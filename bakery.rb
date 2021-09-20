32117acafc505bf5acc375071fa53a645a99d1f1
Andre Malicsi <aj.malicsi@yahoo.com.ph>
2021-09-20 13:02:39 +0800

    adds solution

---
+++ bakery.rb
@@ -0,0 +1,37 @@
+# We have a Confection class representing baked desserts. 
+# Your job is to create a new subclass of Confection, named Cupcake and Banana Cake. 
+# Cupcakes and Banana Cakes need to be baked just like other confections, but cupcakes also need to be frosted afterward. 
+# Write a prepare method for your Cupcake and Banana Cake class that prints "Baking at 350 degrees for 25 minutes.", 
+# and then prints "Applying frosting" for Cupcake only. 
+
+class Confection
+    def prepare
+        puts "Baking at 350 degrees for 25 minutes."
+    end
+
+end
+
+
+class Cupcake < Confection  
+    def prepare
+        super
+        puts "applying frosting"
+    end
+
+end
+
+
+class BananaCake < Confection
+    def prepare
+        super
+    end
+end
+
+bcake = BananaCake.new()
+
+bcake.prepare
+
+cupcake = Cupcake.new()
+
+cupcake.prepare
+
