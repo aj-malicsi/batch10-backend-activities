c3c6d89ce1cc2bae30847682422eea31e2098ac5
Andre Malicsi <aj.malicsi@yahoo.com.ph>
2021-09-23 20:51:54 +0800

    database activities

---
+++ postgres/db_activity_9.23.21.sql
@@ -0,0 +1,68 @@
+CREATE TABLE students(
+	id INT PRIMARY KEY,
+	first_name varchar(40),
+	middle_name varchar(40),
+	last_name varchar(40),
+	age INT,
+	location varchar(40)
+
+)
+
+
+INSERT INTO students(id, first_name, middle_name, last_name, age, location)
+VALUES(1, 'juan', 'Blank', 'cruz', 18, 'Manila');
+
+
+INSERT INTO students(id, first_name, middle_name, last_name, age, location)
+VALUES(2, 'Anne', 'Blank', 'Wall', 20, 'Manila');
+
+
+INSERT INTO students(id, first_name, middle_name, last_name, age, location)
+VALUES(3, 'Theresa', 'Blank', 'Joseph', 21, 'Manila');
+
+
+INSERT INTO students(id, first_name, middle_name, last_name, age, location)
+VALUES(4, 'Issac', 'Blank', 'Gray', 19, 'Laguna');
+
+
+INSERT INTO students(id, first_name, middle_name, last_name, age, location)
+VALUES(5, 'Zack', 'Blank', 'Matthews', 22, 'Marikina');
+
+INSERT INTO students(id, first_name, middle_name, last_name, age, location)
+VALUES(6, 'Finn', 'Blank', 'Lam', 25, 'Manila');
+
+
+SELECT * FROM students
+
+
+UPDATE students
+SET first_name = 'Ivan',
+	middle_name = 'Ingram',
+	last_name = 'Howard',
+	age = 25,
+	location = 'Bulacan'
+WHERE id = 1
+
+DELETE FROM students
+WHERE id = BASE64
+
+
+-- display count of all students
+SELECT 
+	COUNT(id)
+FROM
+	students
+	
+-- select all students with manila as location
+	
+SELECT * FROM students WHERE location = 'Manila'
+
+-- display average age of all students
+
+SELECT AVG(age) FROM students
+
+-- display all students by age descending order
+
+SELECT * FROM students ORDER BY age DESC
+	
+
